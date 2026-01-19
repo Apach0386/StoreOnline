c0480ad4a7b7b40e78b74a72159598154e6a90ef
Yurii Derkach <89613321+Apach0386@users.noreply.github.com>
2026-01-19 21:29:23 +0200

    add producer

--- StoreOnline/StoreOnline.Domain.DependencyInjection/ServiceColectionExtensions.cs
+++ StoreOnline/StoreOnline.Domain.DependencyInjection/ServiceColectionExtensions.cs
@@ -1,19 +1,30 @@
-﻿using Domain.Exceptions;
+﻿using Confluent.Kafka;
+using Domain.Exceptions;
 using FluentValidation;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using StoreOnline.Domain.Pipelines;
 
 namespace Domain.Dependency.Injection;
 
 public static class ServiceColectionExtensions
 {
-    public static IServiceCollection AddDomain(this IServiceCollection services)
+    public static IServiceCollection AddDomain(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddMediatR(config => config
         .AddOpenBehavior(typeof(MonitoringPipeline<,>))
         .AddOpenBehavior(typeof(TransactionPipeline<,>))
         .RegisterServicesFromAssembly(typeof(DomainException).Assembly));
         services.AddValidatorsFromAssemblyContaining<DomainException>(includeInternalTypes: true);
+        services.AddSingleton<IProducer<Null, string>>(sp =>
+        {
+            var config = new ProducerConfig
+            {
+                BootstrapServers = configuration.GetConnectionString("kafka")
+            };
+            return new ProducerBuilder<Null, string>(config).Build();
+        });
 
 
 
--- StoreOnline/StoreOnline.Domain/UseCases/ProductUseCases/Commands/Create/CreateProductCommandHandler.cs
+++ StoreOnline/StoreOnline.Domain/UseCases/ProductUseCases/Commands/Create/CreateProductCommandHandler.cs
@@ -1,4 +1,5 @@
-﻿using Domain.UseCases.ProductUseCases.Abstract;
+﻿using Confluent.Kafka;
+using Domain.UseCases.ProductUseCases.Abstract;
 using Domain.UseCases.ProductUseCases.Models;
 using MediatR;
 
@@ -7,13 +8,35 @@ namespace StoreOnline.Domain.UseCases.ProductUseCases.Commands.Create;
 public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, ProductModel>
 {
     private readonly IProductStorage _storage;
+    private readonly Confluent.Kafka.IProducer<Null, string> _producer;
 
-    public CreateProductCommandHandler(IProductStorage storage)
+    public CreateProductCommandHandler(IProductStorage storage, IProducer<Null, string> producer)
     {
         _storage = storage;
+        _producer = producer;
     }
-    public Task<ProductModel> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    public async Task<ProductModel> Handle(CreateProductCommand request, CancellationToken cancellationToken)
     {
-       return _storage.Create(request, cancellationToken);
+
+        var productModel = await _storage.Create(request, cancellationToken);
+
+        CreateProductMessage productMessage = new CreateProductMessage
+        {
+            Id = productModel.Id,
+            Name = request.Name!,
+            Description = request.Description
+        };
+
+        var jSon = System.Text.Json.JsonSerializer.Serialize(productMessage);
+
+        var message = new Message<Null, string>
+        {
+            Value = jSon
+        };
+
+        await _producer.ProduceAsync("create-product-topic", message);
+
+        return productModel;
+
     }
 }
